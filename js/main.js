1c281e662c8de203cff3c0b6de49d821386bdd5c
Tito Arevalo <titotono00@hotmail.es>
2021-06-01 23:14:04 -0600

    3

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
     $('.collapsible').collapsible();
-    $(".venobox").venobox({
-        framewidth : '400px',
-        frameheight: '300px',
-    }); 
+    $(".venobox").venobox(); 
 });
-var glideMulti = new Glide(".multi", {
-    type: 'slider',
-    autoplay: 4000,
-    focusAt: 'center',
-    perView: 2
-  });
-  glideMulti.mount();
+
