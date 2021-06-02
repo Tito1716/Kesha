18bd9850d951772eb336932d1678ca2a540e8469
Tito Arevalo <titotono00@hotmail.es>
2021-06-01 22:26:01 -0600

    2

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ $(document).ready(function(){
         frameheight: '300px',
     }); 
 });
+var glideMulti = new Glide(".multi", {
+    type: 'slider',
+    autoplay: 4000,
+    focusAt: 'center',
+    perView: 2
+  });
+  glideMulti.mount();
